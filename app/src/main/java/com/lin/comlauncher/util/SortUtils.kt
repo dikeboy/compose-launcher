cc183dcc9cc1764bf89598b92d40cc439ee00152
dikeboy <874720637@qq.com>
2021-09-13 11:04:57 +0800

    update drag icon

--- app/src/main/java/com/lin/comlauncher/util/SortUtils.kt
+++ app/src/main/java/com/lin/comlauncher/util/SortUtils.kt
@@ -5,21 +5,34 @@ import androidx.compose.ui.unit.dp
 import com.lin.comlauncher.entity.ApplicationInfo
 
 object SortUtils {
-    fun resetPos(list:SnapshotStateList<ApplicationInfo>
+    fun resetPos(list:SnapshotStateList<ApplicationInfo>,app:ApplicationInfo
     ){
+        run outside@{
+            var preY = 80
+            list.forEach {
+                if(app==it)
+                    return@forEach
+                if(app.posY<it.posY){
+                    app.posY = preY
+                    return@outside
+                }
+                preY = it.posY
+            }
+            app.posY = preY
+        }
+
         list.sortWith(comparator = { a1, a2->
             if(a1.posY==a2.posY){
               a1.posX-a2.posX
             }else{
                 a1.posY-a2.posY
             }
-
         })
 
         list.forEachIndexed { index, ai ->
             ai.posX = (index%4)*ai.width
             ai.posY = index/4*100+80
-            LogUtils.e("pos=${ai.posX} posY=${ai.posY} name=${ai.name}")
+//            LogUtils.e("pos=${ai.posX} posY=${ai.posY} name=${ai.name}")
         }
     }
 }
