b785db9c058d6e03037c6ae517a708fb888e2b39
dikeboy <874720637@qq.com>
2021-09-15 16:20:06 +0800

    Update SortUtils.kt

--- app/src/main/java/com/lin/comlauncher/util/SortUtils.kt
+++ app/src/main/java/com/lin/comlauncher/util/SortUtils.kt
@@ -7,7 +7,7 @@ import com.lin.comlauncher.entity.CellBean
 
 object SortUtils {
     fun calculPos(
-        list: SnapshotStateList<ApplicationInfo>, app: ApplicationInfo
+        list: ArrayList<ApplicationInfo>, app: ApplicationInfo
     ) {
         var currentPos = findCurrentCell(app.posX, app.posY)
         var isEmpty = true
@@ -32,7 +32,7 @@ object SortUtils {
     }
 
     fun resetChoosePos(
-        list: SnapshotStateList<ApplicationInfo>, app: ApplicationInfo
+        list: ArrayList<ApplicationInfo>, app: ApplicationInfo
     ) {
         list.forEach {
             if (app == it)
@@ -42,22 +42,29 @@ object SortUtils {
         }
 
         var currenPos = findCurrentCell(app.posX, app.posY)
+        var prePos = findCurrentCell(app.orignX,app.orignY)
         LogUtils.e("currentPos=$currenPos")
         if (currenPos < 0)
             currenPos = 0
         else if (currenPos >= 20)
             currenPos = 19
-        list.remove(app)
-        list.add(currenPos, app)
+//        list.remove(app)
+//        list.add(currenPos, app)
 
-        list.forEachIndexed { index, ai ->
-            if (index == currenPos)
+        app.cellPos = currenPos
+        list.forEachIndexed { pos, ai ->
+            if (ai == app)
                 return@forEachIndexed
+            var index = if(ai.cellPos>prePos) ai.cellPos-1 else ai.cellPos
+            if(ai.cellPos>currenPos){
+                index = ai.cellPos
+            }
             ai.orignX = (index % 4) * ai.width
             ai.orignY = index / 4 * LauncherConfig.HOME_CELL_HEIGHT + LauncherConfig.DEFAULT_TOP_PADDING
             ai.needMoveX = ai.posX - ai.orignX
             ai.needMoveY = ai.posY - ai.orignY
             ai.cellPos = index
+            index++
 //            LogUtils.e("pos=${ai.posX} posY=${ai.posY} desx=${ai.orignX} desy=${ai.orignY} name=${ai.name}")
         }
     }
