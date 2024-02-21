1c8f2cbf8b99b000b8f5e97db9681108420cb5d0
dikeboy <874720637@qq.com>
2024-02-21 17:13:07 +0800

    Update LauncherConfig.kt

--- app/src/main/java/com/lin/comlauncher/util/LauncherConfig.kt
+++ app/src/main/java/com/lin/comlauncher/util/LauncherConfig.kt
@@ -4,20 +4,24 @@ object LauncherConfig {
     val DEFAULT_TOP_PADDING = 80
     val HOME_CELL_HEIGHT = 100
     var HOME_CELL_WIDTH = 0
+    var HOME_CELL_LEFT_PADDING = 20
     var HOME_WIDTH = 0;
     var HOME_HEIGHT = 0;
     var HOME_TOOLBAR_START= 100
     var POSITION_HOME = 1;
     var POSITION_TOOLBAR = 2;
+    var POSITION_FOLD = 3;
     var CELL_ICON_WIDTH = 56
     var HOME_DEFAULT_PADDING_LEFT = 10;
     var HOME_DRAG_PAGGING_PADDING = 20;
     var HOME_PAGE_CELL_NUM  = 18;
     var HOME_PAGE_CELL_MAX_NUM  = 20;
+    var HOME_FOLDER_HEIGHT = 320;
 
     var CELL_POS_HOME_LEFT = -10;
     var CELL_POS_HOME_RIGHT = -11;
 
     var CELL_TYPE_APP = 0;
     var CELL_TYPE_FOLD = 1;
+
 }
